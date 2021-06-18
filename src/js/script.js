b2ace3cc32569616f9c64efef140f8522e025fbf
KubanovValentin <delo.biss13@gmail.com>
2021-06-18 21:26:33 +0300

    we continue

--- src/js/script.js
+++ src/js/script.js
@@ -37,8 +37,21 @@ $(document).ready(function() {
         $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
     });
 
+    $(document).ready(function() {
+      $('.carousel__inner').slick({
+          speed: 1200,
+          // adaptiveHeight: true,
+          prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+          nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+          
+      });
 
-
+    window.addEventListener("mousewheel", function(e) {
+    if (e.ctrlKey) {
+      e.preventDefault();
+      return false;
+    }
+  });
 
 
 });
@@ -47,7 +60,4 @@ $(document).ready(function() {
 
 // fps counter created by: https://gist.github.com/sharkbrainguy/1156092,
 // no need to create my own :)
-
-
-
-
+});
