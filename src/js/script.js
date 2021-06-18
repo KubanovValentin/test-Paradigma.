46f7e2f4ed02422c863ddb13de78fdc25925fd77
KubanovValentin <delo.biss13@gmail.com>
2021-06-18 12:39:11 +0300

    add fail-js

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,34 @@
-// $(document).ready(function () {
-//     $(".button.rus_lang").click(function () {
-//         //alert("Нажата кнопка АНГЛИЙСКИЙ");
-//         var rusLang = document.getElementsByClassName("rus_lang");
-//         var enLang = document.getElementsByClassName("en_lang");
-//         for (i = 0; i < enLang.length; i++) {
-//             enLang[i].style.position = "static";
-//             rusLang[i].style.position = "absolute";
-//             rusLang[i].style.top = "-9999px";
-//             rusLang[i].style.left = "-9999px";
-//         }
-//     });
+$(document).ready(function() {
+    $(".button.rus_lang").click(function () {
+        //alert("Нажата кнопка АНГЛИЙСКИЙ");
+        var rusLang = document.getElementsByClassName("rus_lang");
+        var enLang = document.getElementsByClassName("en_lang");
+        for (i = 0; i < enLang.length; i++) {
+          enLang[i].style.position = "static";
+          rusLang[i].style.position = "absolute";
+          rusLang[i].style.top = "-9999px";
+          rusLang[i].style.left = "-9999px";
+        }
+      });
+      
+      $(".button.en_lang").click(function () {
+        //alert("Нажата кнопка АНГЛИЙСКИЙ");
+        var rusLang = document.getElementsByClassName("rus_lang");
+        var enLang = document.getElementsByClassName("en_lang");
+        for (i = 0; i < enLang.length; i++) {
+          rusLang[i].style.position = "static";
+          enLang[i].style.position = "absolute";
+          enLang[i].style.top = "-9999px";
+          enLang[i].style.left = "-9999px";
+        }
+      });
+});
 
-//     $(".button.en_lang").click(function () {
-//         //alert("Нажата кнопка АНГЛИЙСКИЙ");
-//         var rusLang = document.getElementsByClassName("rus_lang");
-//         var enLang = document.getElementsByClassName("en_lang");
-//         for (i = 0; i < enLang.length; i++) {
-//             rusLang[i].style.position = "static";
-//             enLang[i].style.position = "absolute";
-//             enLang[i].style.top = "-9999px";
-//             enLang[i].style.left = "-9999px";
-//         }
-//     });
-// });
+
+
+// fps counter created by: https://gist.github.com/sharkbrainguy/1156092,
+// no need to create my own :)
+
+
+
+
