8be425b84a6dd524b22b909ec1d92f42f7296a28
KubanovValentin <delo.biss13@gmail.com>
2021-06-18 10:31:30 +0300

    add fonts

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,26 +1,25 @@
-function validateForms(form) {
-    $(form).validate({
-        rules: {
-            phone: "required",
-        },
-        messages: {
+// $(document).ready(function () {
+//     $(".button.rus_lang").click(function () {
+//         //alert("Нажата кнопка АНГЛИЙСКИЙ");
+//         var rusLang = document.getElementsByClassName("rus_lang");
+//         var enLang = document.getElementsByClassName("en_lang");
+//         for (i = 0; i < enLang.length; i++) {
+//             enLang[i].style.position = "static";
+//             rusLang[i].style.position = "absolute";
+//             rusLang[i].style.top = "-9999px";
+//             rusLang[i].style.left = "-9999px";
+//         }
+//     });
 
-            phone: "Пожалуйста, введите свой номер телефона",
-            
-        }
-    });
-};
-
-$('form').submit(function(e) {
-    e.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: "mailer/smart.php",
-        data: $(this).serialize()
-    }).done(function() {
-        $(this).find("input").val("");
-
-        $('form').trigger('reset');
-    });
-    return false;
-  });
+//     $(".button.en_lang").click(function () {
+//         //alert("Нажата кнопка АНГЛИЙСКИЙ");
+//         var rusLang = document.getElementsByClassName("rus_lang");
+//         var enLang = document.getElementsByClassName("en_lang");
+//         for (i = 0; i < enLang.length; i++) {
+//             rusLang[i].style.position = "static";
+//             enLang[i].style.position = "absolute";
+//             enLang[i].style.top = "-9999px";
+//             enLang[i].style.left = "-9999px";
+//         }
+//     });
+// });
