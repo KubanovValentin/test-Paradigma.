0bd7820085205b5bacd90d83acdd50b3df398036
KubanovValentin <delo.biss13@gmail.com>
2021-06-19 22:38:55 +0300

    test almost finished

--- dist/js/script.js
+++ dist/js/script.js
@@ -37,4 +37,23 @@ $(document).ready(function() {
         $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
     });
 
+    window.addEventListener('DOMContentLoaded', () => {
+      const menu = document.querySelector('.menu'),
+          menuItem = document.querySelectorAll('.menu__item'),
+          hamburger = document.querySelector('.hamburger');
+    
+      hamburger.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('menu_active');
+      });
+    
+      menuItem.forEach(item => {
+          item.addEventListener('click', () => {
+              hamburger.classList.toggle('hamburger_active');
+              menu.classList.toggle('menu_active');
+          })
+      })
+    });
+
+
 });
